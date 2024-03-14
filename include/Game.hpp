858d4992b818a1d590e938db5add1276ecf79339
malevitzch <malevitzch@gmail.com>
2024-03-14 14:18:25 +0100

    added background and implemented user-controlled camera movement

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,17 +1,25 @@
 #ifndef GAME
 #define GAME
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
 class GameState
 {
   private:
   public:
 };
 
+sf::Vector2f normalize(sf::Vector2f v);
+sf::Vector2f clamp(sf::Vector2f v, sf::Vector2f xbounds, sf::Vector2f ybounds);
 class Game
 {
   private:
+    sf::Texture map_texture;
+    sf::Sprite map;
+    sf::Clock clock;
     sf::RenderWindow window;
-    void processEvent(sf::Event event); 
+    void processEvent(sf::Event event);
+    void physicsUpdate(float dt);
   public:
     Game();
     Game(GameState game_state);
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,7 +1,26 @@
 #include "../include/Game.hpp"
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <thread>
+#include <algorithm>
+#include <chrono>
+sf::Vector2f normalize(sf::Vector2f v)
+{
+  float len = std::sqrt(v.x * v.x + v.y * v.y);
+  if(len == 0.0)
+  {
+    return {0.0, 0.0};
+  }
+  v.x /= len;
+  v.y /= len;
+  return v;
+}
+sf::Vector2f clamp(sf::Vector2f v, sf::Vector2f xbounds, sf::Vector2f ybounds)
+{
+  return {std::min(std::max(xbounds.x, v.x), xbounds.y), std::min(std::max(ybounds.x, v.y), ybounds.y)};
+}
 
-void Game::processEvent(sf::Event event) //todo: replace void with a type that holds user input
+void Game::processEvent(sf::Event event) //TODO: replace void with a type that holds user input
 {
   if(event.type == sf::Event::Closed)
   {
@@ -20,20 +39,55 @@ void Game::processEvent(sf::Event event) //todo: replace void with a type that h
   }
 }
 
+void Game::physicsUpdate(float dt)
+{
+  sf::Vector2f movement = {0, 0};
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+  {
+    movement.y -= 1.0;
+  }
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+  {
+    movement.y += 1.0;
+  }
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+  {
+    movement.x -= 1.0;
+  }
+  if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+  {
+    movement.x += 1.0;
+  }
+
+  float speed = 5;
+
+  sf::Vector2f transform_vector = normalize({movement.x, movement.y});
+  transform_vector = {transform_vector.x * speed, transform_vector.y * speed}; //TODO: Vector2f scalar multiplication function
+  map.setOrigin(clamp({map.getOrigin().x + transform_vector.x,  map.getOrigin().y + transform_vector.y}, {0, 800}, {0, 800} )); //TODO: create a function that adds Vector2f
+}
 Game::Game() : window(sf::RenderWindow(sf::VideoMode(200, 200), "Game Window")) {}
 void Game::run()
 {  
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  //interesting thing, paths are relative to the main 2d-game directory rather than the executable or the cpp file they are referenced invoid Game::processEvent(sf::Event event) 
+  if(!map_texture.loadFromFile("assets/TestMap.png")) //TODO: add a library function that exctracts image assets by name eg. getImageAsset("TestMap")
+  {
+    std::cout<<"ERROR LOADING MAP";
+    throw;
+  }
+  map.setTexture(map_texture);
   while (window.isOpen())
   {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
     sf::Event event;
     while (window.pollEvent(event))
     {
       processEvent(event);
     }
+    float dt = clock.getElapsedTime().asSeconds();
+    physicsUpdate(dt);
+
     window.clear();
-    window.draw(shape);
+    window.draw(map);
     window.display();
   }
 }
